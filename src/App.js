823b6be59c685426e00fb7b70f43825efca902f5
mitsui3point <rladbsgh0331@gmail.com>
2023-09-29 16:46:43 +0900

    feat: update refactor components - 컴포넌트를 만드는 기술인 리액트 덕분에 개발자는 하나의 독립된 부품으로 태그를 만들수 있게 되었다. - 이 부품을 이용하게 되면 더 적은 복잡도로 소프트웨어를 만들 수 있게 된다. - 이 부품을 이용하면 컴포넌트를 다른 사람에게 공유할 수도 있다. - 다른 사람이 만든 컴포넌트를 내가 사용할 수도 있다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+/**
+ * 컴포넌트(사용자 정의 태그)
+ *  1.함수형
+ *  2.반드시 첫글자 대문자
+ * @returns component
+ */
+function Header() {
+  return <header>
+    <h1><a href='/'>WEB</a></h1>
+  </header>
+}
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/2'>css</a></li>
+      <li><a href='/read/3'>js</a></li>
+    </ol>
+  </nav>
+}
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+function App() { 
   return (
     <div className="App">
-      hello react!
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
